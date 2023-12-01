f130bf32a65c4f1a562a6c30bc9f3e515dd3fb60
Amuro-R <yxlc3000@gmail.com>
2023-12-01 11:18:18 +0800

    配置

--- src/main/java/org/edelweiss/logging/EdelweissLoggingAutoConfiguration.java
+++ src/main/java/org/edelweiss/logging/EdelweissLoggingAutoConfiguration.java
@@ -1,13 +1,16 @@
 package org.edelweiss.logging;
 
+import org.edelweiss.logging.annotation.EnableELog;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Amuro-R
  * @date 2023/11/20
  **/
+@EnableELog
 @Configuration
 public class EdelweissLoggingAutoConfiguration {
+
 
 
 }
